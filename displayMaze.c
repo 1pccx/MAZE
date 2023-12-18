5665a39cc0e2036c1bdf2b7416d332ad27027152
1pccx <121043242+1pccx@users.noreply.github.com>
2023-12-18 12:39:58 +0800

    Add files via upload

---
+++ displayMaze.c
@@ -0,0 +1,36 @@
+#include "maze.h"
+
+// Åã¥Ü°g®c
+void displayMaze() 
+{
+#ifdef _WIN32
+    system("cls");
+#elif defined __linux__
+    system("clear");
+#endif
+
+    for (int i = 0; i < ROWS; i++) 
+    {
+        for (int j = 0; j < COLS; j++) 
+        {
+            if (i == playerRow && j == playerCol) 
+            {
+                printf("@ ");
+            }
+            else if (i == exitRow && j == exitCol) 
+            {
+                printf("! ");
+            }
+            else if (maze[i][j] == 1) 
+            {
+                printf("# ");
+            }
+            else 
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
