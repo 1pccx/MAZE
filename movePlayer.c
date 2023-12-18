9c49dee29be515f2aa84ba3859a62c0733737e43
1pccx <121043242+1pccx@users.noreply.github.com>
2023-12-18 12:33:14 +0800

    Add files via upload

---
+++ movePlayer.c
@@ -0,0 +1,34 @@
+#include "maze.h"
+
+// ²¾°Êª±®a
+void movePlayer(char direction) {
+    switch (direction) {
+    case 'W':
+    case 'w':
+        if (playerRow > 0 && maze[playerRow - 1][playerCol] == 0) {
+            playerRow--;
+        }
+        break;
+
+    case 'S':
+    case 's':
+        if (playerRow < ROWS - 1 && maze[playerRow + 1][playerCol] == 0) {
+            playerRow++;
+        }
+        break;
+
+    case 'A':
+    case 'a':
+        if (playerCol > 0 && maze[playerRow][playerCol - 1] == 0) {
+            playerCol--;
+        }
+        break;
+
+    case 'D':
+    case 'd':
+        if (playerCol < COLS - 1 && maze[playerRow][playerCol + 1] == 0) {
+            playerCol++;
+        }
+        break;
+    }
+}
